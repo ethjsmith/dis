1f9be1f19c27b6748cc6b837c7d94f5995dc3dd9
Ethan Smith <ethan@esmithy.net>
2021-11-09 12:27:01 -0700

    mod

--- audio.js
+++ audio.js
@@ -1,19 +1,21 @@
 var fs = require('fs');
-var portAudio = require('../index.js');
+var portAudio = require('node-portaudio');
 
-// Create an instance of AudioIO with inOptions (defaults are as below), which will return a ReadableStream
-var ai = new portAudio.AudioIO({
-  inOptions: {
-    channelCount: 2,
-    sampleFormat: portAudio.SampleFormat16Bit,
-    sampleRate: 44100,
-    deviceId: -1, // Use -1 or omit the deviceId to select the default device
-    closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
-  }
+console.log(portAudio.getDevices());
+
+// Create an instance of AudioInput, which is a ReadableStream
+const ai = new portAudio.AudioInput({
+  channelCount: 2,
+  sampleFormat: portAudio.SampleFormat16Bit,
+  sampleRate: 44100,
+  deviceId : -1 // Use -1 or omit the deviceId to select the default device
 });
 
+// handle errors from the AudioInput
+ai.on('error', err => console.error);
+
 // Create a write stream to write out to a raw audio file
-var ws = fs.createWriteStream('rawAudio.raw');
+const ws = fs.createWriteStream('rawAudio.raw');
 
 //Start streaming
 ai.pipe(ws);
