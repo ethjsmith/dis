4af065aa500a3c1a8bcb1824457d07db3238512e
Ethan Smith <ethan@esmithy.net>
2021-11-12 10:16:30 -0700

    removed some stuff, and changed a configuration so that MAYBE? now it streams the data correctly

--- t.js
+++ t.js
@@ -36,29 +36,15 @@ client.on("messageCreate", async message => {
   if (message.content.startsWith(`${prefix}play`)) {
     execute(message);
     return;
-  // } else if (message.content.startsWith(`${prefix}skip`)) {
-  //   console.log("got skip");
-  //   skip(message, serverQueue);
-  //   return;
-  // } else if (message.content.startsWith(`${prefix}stop`)) {
-  //   stop(message, serverQueue);
-  //   return;
    } else {
      message.channel.send("You need to enter a valid command!");
    }
 });
-// client.on("messageCreate", (message) => {
-//   if (message.author.bot) return false;
-//
-//   console.log(`Message from ${message.author.username}: ${message.content}`);
-// });
+
+
 async function execute(message) {
   console.log(message);
-  const connection = joinVoiceChannel({
-  	channelId: message.member.voice.channel.id,
-  	guildId: message.guild.id,
-  	adapterCreator: message.guild.voiceAdapterCreator,
-  });
+  const connection = joinVoiceChannel({channelId: message.member.voice.channel.id,guildId: message.guild.id,adapterCreator: message.guild.voiceAdapterCreator,}); // condensed the declarations that work
   var ai = new portAudio.AudioIO({
   inOptions: {
     channelCount: 2,
@@ -66,9 +52,8 @@ async function execute(message) {
     sampleRate: 44100,
     deviceId: 6, // Use -1 or omit the deviceId to select the default device
     closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
-  }
-});
-
+  }});
+// https://amishshah.github.io/prism-media/?api#opus.encodeURIComponent // PRISM DOCS
 // Create a write stream to write out to a raw audio file
 //var ws = fs.createWriteStream('rawAudio.raw');
 
@@ -85,103 +70,12 @@ const transcoder = new prism.FFmpeg({
 });
 const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
 ai.pipe(transcoder).pipe(opus);
-console.log("just checking where it gets to");
+ai.start();
+
 resource = createAudioResource(ai.pipe(transcoder).pipe(opus));
-//ai.start();
 const player = createAudioPlayer();
-player.play(resource); // maybe ? 
-console.log("The code does hit");
-}
-async function execute2(message, serverQueue) {
-  const args = message.content.split(" ");
-  console.log(message.member);
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-    return message.channel.send(
-      "You need to be in a voice channel to play music!"
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return message.channel.send(
-      "I need the permissions to join and speak in your voice channel!"
-    );
-  }
-  //
-  // const songInfo = await ytdl.getInfo(args[1]);
-  // const song = {
-  //       title: songInfo.videoDetails.title,
-  //       url: songInfo.videoDetails.video_url,
-  //  };
-  //
-  if (!serverQueue) {
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true
-    };
-
-    queue.set(message.guild.id, queueContruct);
-
-    queueContruct.songs.push(song);
-
-    try {
-      var connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      console.log(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    return message.channel.send(`${song.title} has been added to the queue!`);
-  }
+player.play(resource); // maybe ?
 }
 
-function skip(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    );
-  if (!serverQueue)
-    return message.channel.send("There is no song that I could skip!");
-  serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    );
-
-  if (!serverQueue)
-    return message.channel.send("There is no song that I could stop!");
-
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
 
 client.login(token);
